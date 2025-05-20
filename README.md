4540c5cd297866e143045a68f2eb6b55c6ee258c
Rémy VALLET <remy.vallet@gmail.com>
2025-05-20 11:25:09 +0200

    Init. Affichage Console

---
+++ README.md
@@ -0,0 +1,37 @@
+# Dungeon Escape
+
+Dungeon Escape est un jeu de donjon textuel en Java où le joueur doit s'échapper en explorant des salles, combattant des ennemis et résolvant des énigmes.
+
+## Fonctionnalités
+
+- Exploration de salles
+- Combat contre des ennemis
+- Gestion d'inventaire
+- Sauvegarde des scores (via MongoDB)
+- Commande en ligne de commande
+
+## Technologies utilisées
+
+- Java 21
+- Maven 3.8
+- Spring Boot 3.4
+- MongoDB 8
+
+## Démarrer le projet
+
+### Extraction du projet
+
+Cloner le dépôt :
+```bash
+git clone https://github.com/rvallet/dungeon-escape.git
+cd dungeon-escape
+```
+
+### Construction du projet
+Construire le projet avec Maven :
+```bash
+mvn clean install
+```
+
+### Licence
+Ce projet est sous licence MIT. Voir le fichier [LICENSE](LICENSE) pour plus de détails.
--- src/main/java/com/rva/dungeon/service/impl/GameServiceImpl.java
+++ src/main/java/com/rva/dungeon/service/impl/GameServiceImpl.java
@@ -1,6 +1,7 @@
 package com.rva.dungeon.service.impl;
 
 import com.rva.dungeon.service.GameService;
+import com.rva.dungeon.utils.console.ConsoleUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,7 +10,26 @@ public class GameServiceImpl implements GameService {
     @Override
     public void startGame() {
 
-        System.out.println("Game started!");
+        boolean gameStarted = true;
+
+        ConsoleUtils.afficher("Game started %s!", "TEST");
+        while (gameStarted) {
+
+            String input = ConsoleUtils.demanderCouleur(ConsoleUtils.BLUE,"Entrez votre commande");
+            switch (input.toLowerCase()) {
+                case "quitter":
+                    gameStarted = false;
+                    ConsoleUtils.afficherCouleur(ConsoleUtils.RED, "Au revoir !");
+                    break;
+                case "explorer":
+                    ConsoleUtils.afficherCouleur(ConsoleUtils.RED, "Vous explorez la salle...");
+                    break;
+                default:
+                    ConsoleUtils.afficherCouleur(ConsoleUtils.RED, "Commande inconnue.");
+                    break;
+            }
+
+        }
 
     }
 
